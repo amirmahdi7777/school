dfe0908cbbfa701628a57ad5c453aa399845d702
abolfazl <amirmahdi_goodarzi@gmail.com>
2023-04-25 02:13:51 -0700

    cldfjglks

--- edit.php
+++ edit.php
@@ -1,33 +1,76 @@
 <!doctype html>
 <html>
 <head>
-    <meta charest="utf-8">
+    <meta charset="utf-8">
     <title>edit</title>
-    <link href="css/style.css" rel="stylesheet"/>
+    <link  rel="stylesheet" href="css/style.css"/>
 </head>
-
 <body>
-<?php
+<?php session_start(); ob_start();
 include "connection.php";
-if (isset($_GET['id']) && isset($_GET['page'])) {
+if (isset($_GET['id']) && isset($_GET['page']) ) {
     $id = $_GET['id'];
     $page = $_GET['page'];
     switch ($page) {
         case 1:
             include "connection.php";
-            $query = "DELETE FROM class WHERE class_id=" . $id;
+            $query = "SELECT * FROM class WHERE class_id=" . $id;
             $result = $db->prepare($query);
             $result->execute();
             $row = $result->fetch(PDO::FETCH_ASSOC);
-            if (isset($_POST['submit'])) {
+            if (isset($_POST['submit1'])) {
 
                 $class_name = $_POST['txt_class'];
-                $query = "UPDATE class SET class_name='" . $class_name . "' WHERE class_id=" . id;
-                $del = $dp->prepare($query);
+                $query = "UPDATE class SET class_name='" . $class_name . "' WHERE class_id=" . $id;
+                $del = $db->prepare($query);
                 $del->execute();
                 header("location:index.php");
             }
+            echo ' <div id="row1_col1">
+<form action="" method="post">
+<fieldest>
+<legend>Class</legend>
+<lable>class name</lable>
+<input type="text" name="txt_class" value="' . $row['class_name'] . '"/>
+<input type="submit" name="submit1" value="edit-class"/>
+</fieldest>
+</form>
+</div>';
+break;
+        case 2:
+            include "connection.php";
+            $query = "SELECT * FROM student WHERE stud_id=" . $id;
+            $result = $db->prepare($query);
+            $result->execute();
+            $row = $result->fetch(PDO::FETCH_ASSOC);
+            if (isset($_POST['submit2'])) {
+                $name = $_POST['stud_name'];
+                $family = $_POST['stud_family'];
+                $ave = $_POST['stud_ave'];
+                $query = "UPDATE student SET name='".$name."'".",family='" . $family . "'" . ",ave=" . $ave . " WHERE stud_id=" . $id;
+                $edit = $db->prepare($query);
+                $edit->execute();
+                header("location:index.php");
+            }
+            echo '
+ <div id="row1_col2">
+<form action="" method="post">
+<fieldest>
+<legend>Student</legend>
+<lable>name</lable></br>
+<input type="text" name="stud_name" value="' . $row['name'] . '"/></br>
+<lable>family</lable></br>
+<input type="text" name="stud_family" value="' . $row['family'] . '"/></br>
+<lable>average</lable></br>
+<input type="text" name="stud_ave" value="' . $row['ave'] . '"/></br>
+<input type="submit" name="submit2" value="edit-student"/>
+</fieldest>
+</form>
+</div>
+';
     }
 }
-echo ' div'
+?>
+</body>
+</html>
 
--- index.php
+++ index.php
@@ -7,12 +7,34 @@
 </head>
 
 <body>
+<?php
+include "connection.php";
+if (isset($_POST['submit1'])){
+    $classid=$_POST['txt_classid'];
+    $classname=$_POST['txt_class'];
+    $ins_sql=" INSERT INTO class(class_id,class_name)  VALUES ('$classid','$classname')";
+    $ins_sql_pre=$db->prepare($ins_sql);
+    $ins_sql_pre->execute();
+}
+if (isset($_POST['submit2'])) {
+    $classid = $_POST['classid'];
+    $stud_id = $_POST['studid'];
+    $stud_name = $_POST['stud_name'];
+    $stud_family = $_POST['stud_family'];
+    $stud_ave = $_POST['stud_ave'];
+    $ins_sql = "INSERT INTO student(stud_id,class_id,name,family,ave)  VALUES (stud_id,'$classid','$stud_name','$stud_family','$stud_ave')";
+    $ins_sql_pre = $db->prepare($ins_sql);
+    $ins_sql_pre->execute();
+}
+?>
 <div id="wrapper">
     <div id="row1">
         <div id="row1_col1">
             <form action="" method="post">
                 <fieldset>
                     <legend>Class</legend>
+                    <label>class id:</label>
+                    <input type="text" name="txt_classid"/>
                     <label>class name:</label>
                     <input type="text" name="txt_class"/>
                     <input type="submit" name="submit1" value="inser-class"/>
@@ -51,13 +73,17 @@
             <form action="" method="post">
                 <fieldset>
                     <legend>Student</legend>
+                    <label>class id</label><br/>
+                    <input type="text" name="classid"/><br/>
+                    <label>stud id</label><br/>
+                    <input type="text" name="studid"/><br/>
                     <label>name</label><br/>
                     <input type="text" name="stud_name"/><br/>
                     <label>family</label><br/>
                     <input type="text" name="stud_family"/><br/>
                     <label>average</label><br/>
                     <input type="text" name="stud_ave"/><br/>
-                    <input type="submit" name="submit1" value="inser-student"/>
+                    <input type="submit" name="submit2" value="inser-student"/>
                 </fieldset>
 
             </form>
@@ -84,8 +110,8 @@
     <td>" . $row['name'] . "</td>
     <td>" . $row['family'] . "</td>
     <td>" . $row['ave'] . "</td>
-    <td><a href='delete.php?id=" . $row['stud_id'] . "&&page=1'>delete</a> </td>
-    <td><a href='edit.php?id=" . $row['class_id'] . "&&page=1'>edit</a> </td>
+    <td><a href='delete.php?id=" . $row['stud_id'] . "&&page=2'>delete</a> </td>
+    <td><a href='edit.php?id=" . $row['stud_id'] . "&&page=2'>edit</a> </td>
     </tr>
     ";
                 }
